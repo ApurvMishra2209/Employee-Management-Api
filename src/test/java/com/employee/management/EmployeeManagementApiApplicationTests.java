e3bd5eae8307380b3397553de08e9a8df162edf4
Apurv Mishra <apurvmca2022@gmail.com>
2023-05-08 01:06:46 +0530

    commented test file

--- src/test/java/com/employee/management/EmployeeManagementApiApplicationTests.java
+++ src/test/java/com/employee/management/EmployeeManagementApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.employee.management;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EmployeeManagementApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.employee.management;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EmployeeManagementApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
