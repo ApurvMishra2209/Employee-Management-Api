2e3b685578959b2ee7926660b601a30a93a13f13
Apurv Mishra <apurvmca2022@gmail.com>
2023-05-09 00:42:30 +0530

    updated sql db flyway.

--- src/main/resources/db/migration/V008__INSERT_EMPLOYEE_SKILLSET.sql
+++ src/main/resources/db/migration/V008__INSERT_EMPLOYEE_SKILLSET.sql
@@ -1,19 +1,19 @@
---INSERT INTO employee_skillset (    mapping_id  ,
---                                   employee_id ,
---                                   skill_set_id)
---VALUES (10000,
---        10000,
---        10000
---        ),
---        (10001,
---                10000,
---                10002
---                ),
---                (10002,
---                        10001,
---                        10002
---                        ),
---                        (10003,
---                                10001,
---                                10003
---                                );
+INSERT INTO employee_skillset (    mapping_id  ,
+                                   employee_id ,
+                                   skill_set_id)
+VALUES (10000,
+        10000,
+        10000
+        ),
+        (10001,
+                10000,
+                10002
+                ),
+                (10002,
+                        10001,
+                        10002
+                        ),
+                        (10003,
+                                10001,
+                                10003
+                                );
